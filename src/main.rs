533c641b4735d14fa7c7b089d4c93dd865dad99f
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-07-30 13:18:11 -0300

    injection works

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+use std::process::Command;
 use std::{path::PathBuf, println};
 use std::collections::HashMap;
 mod helpers;
 
+#[allow(unused_assignments)]
+#[allow(unused_variables)]
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
     let mut dir = PathBuf::from(".");
     let mut env_file_exists: bool = dir.join(".env").exists();
-    println!("ARGS: {:?}", args);
+    //println!("ARGS: {:?}", args);
+
+    if args.len() == 0 {
+        let logo = r#"
+  _____        _      _    _             
+  \_   \_ __  (_) ___| | _| |_ ___  _ __ 
+   / /\/ '_ \ | |/ _ \ |/ / __/ _ \| '__|
+/\/ /_ | | | || |  __/   <| || (_) | |   
+\____/ |_| |_|/ |\___|_|\_\\__\___/|_|   
+            |__/                         
+"#;
+
+        println!("{logo}");
+        println!("Environment variable injector.\n");
+        println!("Usage: injektor <command> [args...]");
+        return;
+    }
 
     loop {
         if !env_file_exists {
@@ -34,10 +53,14 @@ fn main() {
             }
 
             let line_pair: Vec<&str> = line.split("=").collect();
-            map.insert(helpers::kill_quotes(line_pair[0].trim().to_string()), helpers::kill_quotes(line_pair[1].trim().to_string()));
+            map.insert(helpers::kill_quotes(line_pair[0].trim().to_string()),
+                helpers::kill_quotes(line_pair[1].trim().to_string())
+            );
 
         }
-        println!("map {:?}", map);
+        // map ready
+        let cmd = Command::new(&args[0]).args(&args[1..]).envs(&map).status().unwrap();
+        //println!("map {:?}", map);
         break;
     }
 }
