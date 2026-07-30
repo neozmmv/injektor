a6626274b37355c88af147020604b612cd32eeee
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-07-30 11:58:58 -0300

    found env

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::path::Path;
+use std::{path::PathBuf, println};
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
-    let start = Path::new(".");
-    let dir: Vec<_> = std::fs::read_dir(start)
-        .expect("Something went wrong reading the path.")
-        .into_iter().collect();
+    let mut dir = PathBuf::from(".");
+    let mut env_file_exists: bool = dir.join(".env").exists();
     println!("ARGS: {:?}", args);
     println!("CURRENT DIR: {:?}", dir);
+    loop {
+        if !env_file_exists {
+            if !dir.pop() {
+                println!("Could not find any .env file!");
+                break;
+            }
+            env_file_exists = dir.join(".env").exists();
+        }
+        println!("{:?}", env_file_exists);
+        break;
+    }
 }
