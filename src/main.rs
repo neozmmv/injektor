1ff5ed84767b636282fd731bac2a6555735472af
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-07-30 12:33:59 -0300

    map for key value pairs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{path::PathBuf, println};
+use std::collections::HashMap;
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
@@ -6,6 +7,7 @@ fn main() {
     let mut env_file_exists: bool = dir.join(".env").exists();
     println!("ARGS: {:?}", args);
     println!("CURRENT DIR: {:?}", dir);
+
     loop {
         if !env_file_exists {
             if !dir.pop() {
@@ -14,7 +16,30 @@ fn main() {
             }
             env_file_exists = dir.join(".env").exists();
         }
+        
+        // found .env file
         println!("{:?}", env_file_exists);
+        let env_file = std::fs::read_to_string(dir.join(".env")).ok().unwrap();
+        let mut line_vec: Vec<String> = env_file.lines().map(|s| s.to_string()).collect();
+        
+        let mut map: HashMap<String, String> = HashMap::new();
+
+        // for each line in the file
+        for i in 0..line_vec.len() {
+            let line: &str = line_vec[i].trim();
+
+            // empty lines or comments
+            if line.is_empty() || line.starts_with("#") {
+                continue;
+            }
+
+            let line_pair: Vec<&str> = line.split("=").collect();
+            map.insert(line_pair[0].trim().to_string(), line_pair[1].trim().to_string());
+
+            println!("{line}");
+        }
+        println!("map {:?}", map);
+        println!("lines: {:?}", line_vec);
         break;
     }
 }
